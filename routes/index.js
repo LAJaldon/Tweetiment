7f9ad213134a6e346236c618d2c200b26e6a03c3
Luigi Alexander Jaldon <luigi.jaldon@connect.qut.edu.au>
2020-10-13 23:26:24 +1000

    Added natural module and added code to use this module and enable sentiment analysis on tweets. Also added code to have basic front end design

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 require('dotenv').config();
+var Analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
+var analyzer = new Analyzer("English", stemmer, "afinn");
 
 var client = new Twitter({
   consumer_key: 'knQr4ZDXfO6gMQUjPc1249l2Q',
@@ -28,21 +31,70 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/twitter', function (req, res) {
-  client.stream('statuses/filter', { track: 'twitter', language: 'en' }, function (stream) {
+  const query = req.query;
+  //let positiveTweets = 0;
+  //let negativeTweets = 0;
+  //let neutralTweets = 0;
+
+  /*client.stream('statuses/filter', { track: query['search'], language: 'en' }, function (stream) {
     stream.on('data', function (tweet) {
       console.log(tweet.text);
+      var wordsArray = tweet.text.split(" ");
+      sentimentScore = analyzer.getSentiment(wordsArray);
+      console.log(sentimentScore);
+      if (sentimentScore > 0) {
+        positiveTweets = positiveTweets + 1;
+      }
+      else if (sentimentScore === 0) {
+        neutralTweets = neutralTweets + 1;
+      }
+      else {
+        negativeTweets = negativeTweets + 1;
+      }
+      res.render('sentimentAnalysis', { positiveTweets, neutralTweets, negativeTweets });
     });
 
     stream.on('error', function (error) {
       console.log(error);
     });
   });
-
-
-  /*client.get('search/tweets', { q: 'lebron', lang: 'en' }, function (error, tweets, response) {
-    console.log(tweets);
-  });
   */
+
+
+
+  client.get('search/tweets', { q: query['search'], lang: 'en', count: '100' }, function (error, tweets, response) {
+    for (var i = 0; i < tweets.statuses.length; i++) {
+      console.log(tweets.statuses[i].text);
+    }
+
+    let positiveTweets = 0;
+    let negativeTweets = 0;
+    let neutralTweets = 0;
+
+    for (var i = 0; i < tweets.statuses.length; i++) {
+      console.log(tweets.statuses[i].text);
+      var wordsArray = tweets.statuses[i].text.split(" ");
+      sentimentScore = analyzer.getSentiment(wordsArray);
+      console.log(sentimentScore);
+      if (sentimentScore > 0) {
+        positiveTweets = positiveTweets + 1;
+      }
+      else if (sentimentScore === 0) {
+        neutralTweets = neutralTweets + 1;
+      }
+      else {
+        negativeTweets = negativeTweets + 1;
+      }
+    }
+
+    console.log("Positive tweets: " + positiveTweets);
+    console.log("Neutral tweets " + neutralTweets);
+    console.log("Negative tweets " + negativeTweets);
+
+    res.render('sentimentAnalysis', { positiveTweets, neutralTweets, negativeTweets });
+  });
+
+
 });
 
 module.exports = router;
