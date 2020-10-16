6c1261044809419748fd2f9253ff96e6059d0ce4
Luigi Alexander Jaldon <luigi.jaldon@connect.qut.edu.au>
2020-10-16 18:34:24 +1000

    added extra layout in front end, added google trends api, also added error handling

--- routes/index.js
+++ routes/index.js
@@ -5,93 +5,105 @@ require('dotenv').config();
 var Analyzer = require('natural').SentimentAnalyzer;
 var stemmer = require('natural').PorterStemmer;
 var analyzer = new Analyzer("English", stemmer, "afinn");
+const googleTrends = require('google-trends-api');
 
 var client = new Twitter({
-  consumer_key: 'knQr4ZDXfO6gMQUjPc1249l2Q',
-  consumer_secret: '0z7kf3sfAwo71nK9BuSbOzakRRkvoZP3Ax9ib0VmORbeYDEasn',
-  access_token_key: '1309335867861823488-w4rSthuWv7ARhCg1Tmxu12OrenHp7t',
-  access_token_secret: '0zQ002iyBIjs3lkbNvtoopz3qEOW9y8J0GecS9Aq3uDrx'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-
-/*
-  TWITTER_CONSUMER_KEY = knQr4ZDXfO6gMQUjPc1249l2Q;
-  TWITTER_CONSUMER_SECRET =  0z7kf3sfAwo71nK9BuSbOzakRRkvoZP3Ax9ib0VmORbeYDEasn;
-  TWITTER_ACCESS_TOKEN_KEY =  1309335867861823488-w4rSthuWv7ARhCg1Tmxu12OrenHp7t;
-  TWITTER_ACCESS_TOKEN_SECRET = 0zQ002iyBIjs3lkbNvtoopz3qEOW9y8J0GecS9Aq3uDrx; 
-
-  consumer_key: 'knQr4ZDXfO6gMQUjPc1249l2Q',
-  consumer_secret: '0z7kf3sfAwo71nK9BuSbOzakRRkvoZP3Ax9ib0VmORbeYDEasn',
-  access_token_key: '1309335867861823488-w4rSthuWv7ARhCg1Tmxu12OrenHp7t',
-  access_token_secret: '0zQ002iyBIjs3lkbNvtoopz3qEOW9y8J0GecS9Aq3uDrx'
-*/
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  let trendingTopicsArray = new Array(16);
+  let topicsArrayCount = 0;
+
+  googleTrends.realTimeTrends({
+    geo: 'AU',
+    category: 'all',
+  }, function (err, results) {
+    if (err) {
+      res.render('error');
+    } else {
+      const trends = JSON.parse(results);
+
+      for (var i = 0; i < trends.storySummaries.trendingStories.length; i++) {
+        trendingTopicsArray[topicsArrayCount] = trends.storySummaries.trendingStories[i].entityNames[0];
+        //console.log(trends.storySummaries.trendingStories[i].entityNames[0])
+        //console.log(trendingTopicsArray[topicsArrayCount]);
+        //console.log(topicsArrayCount);
+        topicsArrayCount++;
+      }
+
+      res.render('index', { title: 'Tweet Sentiment Analysis', trendingTopicsArray });
+    }
+  });
+
 });
 
 router.get('/twitter', function (req, res) {
   const query = req.query;
-  //let positiveTweets = 0;
-  //let negativeTweets = 0;
-  //let neutralTweets = 0;
-
-  /*client.stream('statuses/filter', { track: query['search'], language: 'en' }, function (stream) {
-    stream.on('data', function (tweet) {
-      console.log(tweet.text);
-      var wordsArray = tweet.text.split(" ");
-      sentimentScore = analyzer.getSentiment(wordsArray);
-      console.log(sentimentScore);
-      if (sentimentScore > 0) {
-        positiveTweets = positiveTweets + 1;
-      }
-      else if (sentimentScore === 0) {
-        neutralTweets = neutralTweets + 1;
-      }
-      else {
-        negativeTweets = negativeTweets + 1;
-      }
-      res.render('sentimentAnalysis', { positiveTweets, neutralTweets, negativeTweets });
-    });
-
-    stream.on('error', function (error) {
-      console.log(error);
-    });
-  });
-  */
-
-
-
-  client.get('search/tweets', { q: query['search'], lang: 'en', count: '100' }, function (error, tweets, response) {
-    for (var i = 0; i < tweets.statuses.length; i++) {
-      console.log(tweets.statuses[i].text);
-    }
-
+  const searchQuery = query['search'];
+  console.log(process.env.TWITTER_ACCESS_TOKEN_KEY);
+  client.get('search/tweets', { q: query['search'], lang: 'en', count: '100', result_type: "mixed" }, function (error, tweets, response) {
+    const tweetsCount = tweets.statuses.length;
     let positiveTweets = 0;
     let negativeTweets = 0;
     let neutralTweets = 0;
+    let positiveTweetsArray = new Array(2);
+    let neutralTweetsArray = new Array(2);
+    let negativeTweetsArray = new Array(2);
+    let positiveArrayCount = 0;
+    let neutralArrayCount = 0;
+    let negativeArrayCount = 0;
 
     for (var i = 0; i < tweets.statuses.length; i++) {
-      console.log(tweets.statuses[i].text);
+      //console.log(tweets.statuses[i].text);
+
       var wordsArray = tweets.statuses[i].text.split(" ");
       sentimentScore = analyzer.getSentiment(wordsArray);
-      console.log(sentimentScore);
+
+      //console.log(sentimentScore);
+
       if (sentimentScore > 0) {
         positiveTweets = positiveTweets + 1;
+        positiveArrayCount++;
+        if (positiveArrayCount < 4) {
+          positiveTweetsArray[positiveArrayCount] = tweets.statuses[i].text;
+        }
       }
       else if (sentimentScore === 0) {
         neutralTweets = neutralTweets + 1;
+        neutralArrayCount++;
+        if (neutralArrayCount < 4) {
+          neutralTweetsArray[neutralArrayCount] = tweets.statuses[i].text;
+        }
       }
       else {
         negativeTweets = negativeTweets + 1;
+        negativeArrayCount++;
+        if (negativeArrayCount < 4) {
+          negativeTweetsArray[negativeArrayCount] = tweets.statuses[i].text;
+        }
       }
     }
 
-    console.log("Positive tweets: " + positiveTweets);
-    console.log("Neutral tweets " + neutralTweets);
-    console.log("Negative tweets " + negativeTweets);
+    //console.log("Positive tweets: " + positiveTweets);
+    //console.log("Neutral tweets " + neutralTweets);
+    //console.log("Negative tweets " + negativeTweets);
 
-    res.render('sentimentAnalysis', { positiveTweets, neutralTweets, negativeTweets });
+    const positiveCalculation = (positiveTweets / tweetsCount) * 100;
+    const neutralCalculation = (neutralTweets / tweetsCount) * 100;
+    const negativeCalulation = (negativeTweets / tweetsCount) * 100;
+    const positiveTweetsPercentage = positiveCalculation.toFixed(2);
+    const neutralTweetsPercentage = neutralCalculation.toFixed(2);
+    const negativeTweetsPercentage = negativeCalulation.toFixed(2);
+
+    res.render('sentimentAnalysis', {
+      positiveTweetsPercentage, neutralTweetsPercentage, negativeTweetsPercentage, searchQuery,
+      tweetsCount, positiveTweetsArray, neutralTweetsArray, negativeTweetsArray
+    });
   });
 
 
